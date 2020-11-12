aa60db21cfb6dc1b91787b718271189dec6b4b30
Isabelle <isa_ozawa@hotmail.com>
2020-11-12 16:23:48 -0300

    Atividade Java

---
+++ Exercício 1/src/Pacote/ClasseEx1.java
@@ -0,0 +1,14 @@
+package Pacote;
+
+public class ClasseEx1 {
+	public static void main(String args[])
+	{
+		String sobreMim = "Meu nome é Isabelle, tenho 23 anos e moro em Campinas. "
+						+ "Sou formada em administração, sou uma pessoal muito Legal. "
+						+ "No meu tempo livre gosto de ver séries e filmes e as vezes cozinhar";
+		
+		
+		System.out.println(sobreMim);
+	}
+
+}
---
+++ Exercício 2/src/Pacote2/ClasseEx2.java
@@ -0,0 +1,13 @@
+package Pacote2;
+
+public class ClasseEx2 {
+	public static void main(String args[])
+	{
+		String habilidades = "A habilidade que eu mais tenho desenvolvido no curso é a comunicação e "
+				+ "trabalho em equipe pois em diversas atividades é necessário lidar com diferentes pessoas"
+				+ "e me comunicar de formas diferentes. A mentalidade é a de persistência pois é uma área totalmente"
+				+ "nova na minha vida e preciso me esforçar para conseguir entender muitas coisas ";
+		System.out.println(habilidades);
+	}
+
+}
